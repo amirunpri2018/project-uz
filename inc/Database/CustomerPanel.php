79aee0d36cba20bac6402a10d5848fe82e8e6311
Sebastian <vergilpl@wp.pl>
2019-06-18 19:02:50 +0200

    Aktualizacja Panelu Klienta
    1. Dodanie podstrony do wyświetlenia wszystkich zapytań
    2. Dodanie wyświetlania wszystkich wiadomości do zapytań

--- inc/Database/CustomerPanel.php
+++ inc/Database/CustomerPanel.php
@@ -116,6 +116,130 @@ class CustomerPanel {
         }
     }
 
+    public function getCustomerQuestions(){
+        $getQuestions = new Connect();
+        $questionsIDs = [];
+
+        try {
+            $getQuestionsList = $getQuestions->query(
+                "SELECT
+                            q.ID AS ID,
+                            q.Temat AS Temat,
+                            q.Data_Utworzenia AS Utworzono,
+                            MAX(qc.Data_Wyslania) AS Ostatnia_odp,
+                            zs.Nazwa AS Status
+                        FROM zap_zapytania AS q
+                            INNER JOIN zap_konwersacja AS qc ON q.ID = qc.ID_Zapytania
+                            INNER JOIN zap_status AS zs ON q.`Status` = zs.ID
+                        WHERE q.ID_Klienta = 4
+                        GROUP BY q.ID;"
+            )->fetchAll();
+
+            echo '<table class="table table-hover">';
+            echo '    <thead class="thead-dark">';
+            echo '        <tr>';
+            echo '            <th scope="col">Temat</th>';
+            echo '            <th scope="col">Utworzono</th>';
+            echo '            <th scope="col">Ostatnia odpowiedź</th>';
+            echo '            <th scope="col">Status</th>';
+            echo '        </tr>';
+            echo '    </thead>';
+            echo '    <tbody>';
+
+            foreach($getQuestionsList as $result){
+                echo '        <tr class="clickable-row" data-href="../panelklienta/zapytania.php?id=' . $result['ID'] . '">';
+                echo '            <th scope="row">' . $result['Temat'] . '</th>';
+                echo '            <td>' . $result['Utworzono'] . '</td>';
+                echo '            <td>' . $result['Ostatnia_odp'] . '</td>';
+                echo '            <td>' . $result['Status'] . '</td>';
+                echo '        </tr>';
+
+                $questionsIDs[] = $result['ID'];
+            }
+
+            echo '    </tbody>';
+            echo '</table>';
+
+
+            if (!isset($_SESSION['ID_Zapytania'])){
+                session_start();
+            }
+            $_SESSION['ID_Zapytania'] = $questionsIDs;
+
+            echo '<script>
+                jQuery(document).ready(function($) {
+                    $(".clickable-row").click(function() {
+                        window.location = $(this).data("href");
+                    });
+                });
+                </script>';
+        } finally {
+            $getQuestions->close();
+        }
+    }
+
+    public function getQuestionDetails($ID_Zapytania){
+        $getQuestion = new Connect();
+        $statusID = -1;
+
+        try {
+            $getDetails = $getQuestion->query(
+                "SELECT
+                            qc.ID_Zapytania AS ID_Zapytania,
+                            q.Temat AS Temat,
+                            (SELECT 
+                                CONCAT(imie, ' ', nazwisko) 
+                            FROM klienci 
+                            WHERE klienci.id = qc.ID_Uzytkownika) AS Uzytkownik,
+                            qc.Data_Wyslania AS Data,
+                            qc.Tresc AS Tresc,
+                            q.Status AS Status
+                        FROM zap_konwersacja AS qc
+                            INNER JOIN zap_zapytania AS q ON q.ID = qc.ID_Zapytania
+                            INNER JOIN zap_status AS zs ON q.`Status` = zs.ID
+                        WHERE q.ID_Klienta = 4 
+                            AND qc.ID_Zapytania = '$ID_Zapytania'
+                        GROUP BY qc.ID"
+            )->fetchAll();
+
+            foreach($getDetails as $result){
+                echo '<table class="table table-hover">';
+                echo '    <thead class="thead-dark">';
+                echo '        <tr>';
+                echo '            <th style="width: 33.33%;" scope="col">Temat: ' . $result['Temat'] . '</th>';
+                echo '            <th style="width: 33.33%;" scope="col">Od: ' . $result['Uzytkownik'] . '</th>';
+                echo '            <th style="width: 33.33%;" scope="col">Data: ' . $result['Data'] . '</th>';
+                echo '        </tr>';
+                echo '    </thead>';
+                echo '    <tbody>';
+                echo '        <tr>';
+                echo '            <td colspan="3">' . $result['Tresc'] . '</td>';
+                echo '        </tr>';
+                echo '    </tbody>';
+                echo '</table>';
+
+                $statusID = $result['Status'];
+            }
+
+            if($statusID < 4){
+                echo '<div class="form-group">';
+                    echo '<table class="table table-hover">';
+                    echo '    <thead class="thead-dark">';
+                    echo '        <tr>';
+                    echo '            <th scope="col">Twoja odpowiedź: </th>';
+                    echo '        </tr>';
+                    echo '    </thead>';
+                    echo '</table>';
+                    
+                    echo '<textarea class="form-control" id="textAreaTrescWiadomosci" rows="3"></textarea>';
+                    echo '<button type="submit" class="btn btn-primary">Wyślij wiadomość</button>';
+                echo '</div>';
+            }
+        } finally {
+            $getQuestion->close();
+        }
+    }
+
     public function changeCustomerData() {
 
     }
